01e0e9648cb3aadb3c95e20e8a19ced7324befe6
NMandrindra <166989645+NMandrindra@users.noreply.github.com>
2024-04-16 14:21:06 +0300

    Update algorithme exercice 2.py

--- algorithme exercice 2.py
+++ algorithme exercice 2.py
@@ -1,17 +1,1 @@
-algorithme_table_de_kargnaux
-Debut_algorithme:
 
-Obtenir la liste des termes mintermes d'une fonction booléenne
-
-Minimisation de la fonction booléenne
-
-Convertir une chaîne de caractères booléenne en une forme logique sympy
-
-Supprime le premier "And"
-
-lire(Entrée de l'utilisateur)
-
-Convertir la fonction booléenne en une forme logique
-
-Minimiser la fonction booléenne
-Fin_algorithme
